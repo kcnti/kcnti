4f0d84d5797e1663b98fa7a23777d6447d4caf45
kcnti <kantinun99998@gmail.com>
2024-03-09 15:01:01 +0700

    data

---
+++ src/data/data.js
@@ -0,0 +1,105 @@
+export const activities = [
+    'ตัวแทนประเทศ เข้าร่วมอบรม Global Cybersecurity Camp 2024',
+    'เข้าร่วมอบรบ การพัฒนา IoT Application ด้วย Node-Red และ ThingsBoard รุ่นที่ 2 (FIBO)',
+    'ทัศนศึกษา ที่สถาบันวิจัย บริการวิชาการและการศึกษาระดับโลกด้านวิทยาการหุ่นยนต์',
+    'ทัศนศึกษา ที่ศูนย์ทดสอบผลิตภัณฑ์ไฟฟ้าและอิเล็กทรอนิกส์​ (PTEC)',
+    'ทัศนศึกษา ที่ KASIKORN Business-Technology Group (KBTG)'
+]
+
+import stdio2023 from '../assets/certificates/stdioctf2023.jpg'
+import tctt2023 from '../assets/certificates/tctt2023.jpg'
+import tctt2022 from '../assets/certificates/tctt2022.jpg'
+import tctt2021 from '../assets/certificates/tctt2021.png'
+import triton from '../assets/certificates/triton.png'
+import htb from '../assets/certificates/CyberApocalypse.png'
+import smartNode from '../assets/certificates/SmartNodeMCU.png'
+import rtafCTF from '../assets/certificates/CTFrtaf.png'
+import wro19 from '../assets/certificates/WRO19.png'
+
+export const competition = [
+    {
+        "STDiO CTF 2023 for GCC": {
+            "ranking": "Top 10 From 200 Players",
+            "certificates": stdio2023,
+            "issuer": "2600 Thailand",
+            "date": "Dec 2023"
+        }
+    },
+    {
+        "Thailand Cyber Top Talent 2023": {
+            "ranking": "2nd Place",
+            "certificates": tctt2023,
+            "issuer": "Nation Cyber Security Agency (NCSA)",
+            "date": "Sep 2023"
+        }
+    },
+    {
+        "CTF by Cisco": {
+            "ranking": "1st Place",
+            "certificates": 'no cert',
+            "issuer": "NCSA Thailand National Cyber Week 2023",
+            "date": "Feb 2023"
+        }
+    },
+    {
+        "Thailand Cyber Top Talent 2022": {
+            "ranking": "4th Place",
+            "certificates": tctt2022,
+            "issuer": "Nation Cyber Security Agency (NCSA)",
+            "date": "Oct 2022"
+        }
+    },
+    {
+        "Thailand Cyber Top Talent 2021": {
+            "ranking": "2nd Place",
+            "certificates": tctt2021,
+            "issuer": "Nation Cyber Security Agency (NCSA)",
+            "date": "Nov 2021"
+        }
+    },
+    {
+        "Triton Junior Entreprener Accelerator": {
+            "ranking": "Top 6 From 200 Teams",
+            "certificates": triton,
+            "issuer": "Base Playhouse",
+            "date": "Aug 2021"
+        }
+    },
+    {
+        "HackTheBox Cyber Apocalypse CTF 2021": {
+            "ranking": "Participated",
+            "certificates": htb,
+            "issuer": "HTB",
+            "date": "April 2021"
+        }
+    },
+    {
+        "ค้นหาสุดยอดฝีมือโทรคมนาคมและไอซีทีเทิดพระเกียรติ ครั้งที่ 13 สาขา IoT (Smart NodeMCU)": {
+            "ranking": "2nd Place",
+            "certificates": smartNode,
+            "issuer": "มูลนิธิสภาวิทยาศาสตร์และเทคโนโลยีแห่งประเทศไทย (FSTT)",
+            "date": "Nov 2020"
+        }
+    },
+    {
+        "Royal Thai Air Force Junior CTF 2020": {
+            "ranking": "2nd Place",
+            "certificates": rtafCTF,
+            "issuer": "Royal Thai Air Force",
+            "date": "Aug 2019"
+        }
+    },
+    {
+        "World Robot Olympiad (WRO) 2019": {
+            "ranking": "5th Place",
+            "certificates": wro19,
+            "issuer": "World Robot Olympiad Thailand",
+            "date": "Jul 2019"
+        }
+    },
+
+]
+
+export const projects = [
+
+]
