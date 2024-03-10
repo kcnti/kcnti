ae86a452d8d78623df7cb7b0825f8a4f4a29a836
kcnti <kantinun99998@gmail.com>
2024-03-10 07:21:26 +0700

    data

---
+++ src/data/blog.js
@@ -0,0 +1,10 @@
+export const blogs = [
+    {
+        "GCC2024": {
+            title: "GCC2024",
+            desc: "Global Cyber Security Camp",
+            date: "10 Mar 2024",
+            markdown: "GCC2024.md"
+        }
+    }
+]
